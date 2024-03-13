71af8796082f936b43b9ec6afaab814dd306ef0b
PranjalMantri <pranjalmantri@gmail.com>
2024-03-13 10:56:07 +0530

    made changes to how zone polygon is generated

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-import cv2
 import argparse
-from ultralytics import YOLO
+import cv2
 import supervision as sv
 import numpy as np
+from ultralytics import YOLO
 
 ZONE_POLYGON = np.array([[0, 0], [0.5, 0], [0.5, 1], [0, 1]])
 
 
 def parse_arguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument("--webcam-resolution", nargs=2, default=[480, 640], type=int)
+    parser.add_argument("--webcam-resolution", default=[480, 640], nargs=2, type=int)
 
     args = parser.parse_args()
     return args
@@ -20,30 +20,31 @@ def main():
     frame_width, frame_height = args.webcam_resolution
 
     cap = cv2.VideoCapture(0)
-
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
 
     model = YOLO("yolov8l.pt")
 
-    box_annotator = sv.BoxAnnotator(thickness=1, text_thickness=1, text_scale=1)
+    box_annotator = sv.BoxAnnotator(thickness=2, text_thickness=2, text_scale=1)
 
-    zone_polygon = (ZONE_POLYGON * args.webcam_resolution).astype(int)
-    zone = sv.PolygonZone(polygon=zone_polygon, frame_resolution_wh=tuple(args.webcam_resolution))
+    zone_polygon = (ZONE_POLYGON * np.array([frame_height, frame_width])).astype(int)
+    zone = sv.PolygonZone(
+        polygon=zone_polygon,
+        frame_resolution_wh = tuple(args.webcam_resolution)
+        )
+    
     zone_annotator = sv.PolygonZoneAnnotator(
         zone=zone,
-        color=sv.Color.red(),
-        thickness=1,
+        color = sv.Color.red(),
+        thickness=2,
         text_thickness=2,
-        text_scale=1
+        text_scale=2
     )
 
     while True:
         ret, frame = cap.read()
 
-        results = model(frame, agnostic_nms=True)
-        print(results)
-
+        results = model(frame)
         detections = sv.Detections.from_yolov8(results[0])
         detections = detections[detections.class_id != 0]
 
@@ -57,12 +58,11 @@ def main():
         )
 
         zone.trigger(detections=detections)
-        frame = zone_annotator.annotate(scene=frame)
+        frame = zone_annotator.annotate(scene=frame)    
 
         cv2.imshow("Object Detection", frame)
 
         if cv2.waitKey(30) == 27:
-            print(frame.shape)
             break
 
     cap.release()
